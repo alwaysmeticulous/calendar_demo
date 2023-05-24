ebf8de615609ef03aef3bf9368a4a3d0f5226eea
Alex Ivanov <alex@meticulous.ai>
2023-05-24 13:25:09 +0300

    Skip batching

--- packages/trpc/react/trpc.ts
+++ packages/trpc/react/trpc.ts
@@ -77,7 +77,7 @@ export const trpc: CreateTRPCNext<AppRouter, NextPageContext, null> = createTRPC
         }),
         splitLink({
           // check for context property `skipBatch`
-          condition: (op) => !!op.context.skipBatch,
+          condition: (op) => !!op.context.skipBatch || !!process.env.NEXT_PUBLIC_SKIP_BATCH,
           // when condition is true, use normal request
           true: (runtime) => {
             const links = Object.fromEntries(
