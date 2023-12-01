457f39d8b24389b930226538af7596fb22e1cb3f
Que3216 <q@meticulous.ai>
2023-12-01 19:14:44 +0000

    Update LinkIconButton.tsx

--- apps/web/components/ui/LinkIconButton.tsx
+++ apps/web/components/ui/LinkIconButton.tsx
@@ -12,7 +12,7 @@ export default function LinkIconButton(props: LinkIconButtonProps) {
       <button
         type="button"
         {...props}
-        className="text-md hover:bg-emphasis hover:text-emphasis text-default flex items-center rounded-sm px-2 py-1 text-sm font-medium">
+        className="text-md hover:bg-emphasis hover:text-emphasis text-default flex items-center rounded-sm px-20 py-1 text-sm font-medium">
         <props.Icon className="text-subtle h-4 w-4 ltr:mr-2 rtl:ml-2" />
         {props.children}
       </button>
